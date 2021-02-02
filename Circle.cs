3bf5e8cf671ca7bc46dab29a96a4e5905cefe952
Petr D. Novak <petr-novak-donald@protonmail.com>
2021-02-02 17:53:46 +0100

    upload files

---
+++ Circle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCircleClass
+{
+    public class Circle
+    {
+        private double radius;
+        private string color;
+
+        public Circle()
+        {
+            radius = 1.0;
+            color = "red";
+        }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            color = "red";
+        }
+
+        public Circle(double radius, string color)
+        {
+            this.radius = radius;
+            this.color = color;
+
+        }
+
+        public double getRadius()
+        {
+            return radius;
+        }
+
+        public double getArea()
+        {
+            return Math.PI * Math.Sqrt(radius);
+        }
+
+        public string getColor() {
+
+            return color;
+        }
+
+        public void setRadius(double radius) {
+            this.radius = radius;
+        }
+
+        public void setColor(string color) {
+
+            this.color = color;
+        }
+
+        public String toString() {
+            return "Circle[radius=" + radius + " color=" + color + "]";
+        }
+
+
+
+
+    }
+}
---
+++ TestCircle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCircleClass
+{
+   
+    public class TestCircle
+    {
+      public static void Main(string[] args)
+        {
+            Circle c1 = new Circle();
+          
+            Console.WriteLine( "The circle has radius of " + c1.getRadius() + " and area of " + c1.getArea() + " and color is " + c1.getColor());
+            
+            Circle c2 = new Circle(2.0);
+
+            Console.WriteLine("The circle has radius of " + c2.getRadius() + " and area of " + c2.getArea() + " and color is " + c2.getColor());
+
+            Circle c3 = new Circle(3.0, "pink");
+
+            Console.WriteLine("The circle has radius of " + c3.getRadius() + " and area of " + c3.getArea() + " and color is " + c3.getColor());
+
+            //Console.WriteLine(c1.radius); nejde radius je datova slozka tridy Circle ktera je privatni, pristup k ni je mozno jen pres getter a setter
+
+            //c1.radius = 5.0; nejde radius je datova slozka tridy Circle ktera je privatni, pristup k ni je mozno jen pres getter a setter
+
+
+            Circle c4 = new Circle();
+            c4.setRadius(5.5);
+            Console.WriteLine("radius is: " + c4.getRadius());
+            c4.setColor("yellow");
+            Console.WriteLine("color is: " + c4.getColor());
+
+
+            Circle c5 = new Circle(1.0);
+            Console.WriteLine(c5.toString());
+            Console.WriteLine(c5);
+
+            Console.ReadLine();
+
+        }
+    }
+}
